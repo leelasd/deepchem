601ef61d50dd9541b8f97d19eae590021d7923c2
leswing <lilleswing@gmail.com>
2017-02-16 18:20:07 -0800

    Install with python 2.7

--- devtools/jenkins/jenkins.sh
+++ devtools/jenkins/jenkins.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 envname=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1`
-conda create --name $envname python=3.5
+conda create --name $envname python=2.7
 bash scripts/install_deepchem_conda.sh $envname
+pip install tensorflow-gpu==0.12.1
 source activate $envname
 python setup.py install
 
