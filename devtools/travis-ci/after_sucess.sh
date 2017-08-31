18d9399f36e5725e39606b24df7f565e74d51383
leswing <Karl>
2017-08-31 18:07:25 -0400

    GOGO

--- devtools/travis-ci/after_sucess.sh
+++ devtools/travis-ci/after_sucess.sh
@@ -1,14 +1,3 @@
-echo '$TRAVIS_PULL_REQUEST $TRAVIS_BRANCH'
-echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
-
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-    echo "This is a pull request. No deployment will be done."; exit 0
-fi
-
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-    echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
-fi
-
 # Create the docs and push them to S3
 # -----------------------------------
 echo "About to install numpydoc, s3cmd"
