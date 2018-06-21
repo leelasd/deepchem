9083aa50aa9e0b7ab7a1e1e48df640114250eba8
leswing <lilleswing@gmail.com>
2018-06-21 11:40:28 -0400

    test_script los to dir

--- examples/tests.py
+++ examples/tests.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import tempfile
+import delegator
 
 
 def _example_run(path):
@@ -28,12 +29,32 @@ def test_adme():
 
 
 def test_tox21_fcnet():
-
   print("Running tox21_fcnet()")
   output = _example_run("./tox21/tox21_fcnet.py")
   print(output)
 
 
 if __name__ == "__main__":
-  test_tox21_fcnet()
-  test_adme()
+  if os.path.exists('example_tests'):
+    os.rmdir('example_tests')
+  os.mkdir('example_tests')
+  folders = list(filter(lambda x: os.path.isdir(x), os.listdir('./')))
+  for folder in folders:
+    print(folder)
+    if folder != 'tox21':
+      continue
+    os.chdir(folder)
+    files = list(filter(lambda x: x.endswith('.py'), os.listdir('./')))
+    for f in files:
+      cmd = "python %s" % f
+      print(cmd)
+      c = delegator.run(cmd)
+
+      outfile = os.path.basename(f) + ".out"
+      with open(outfile, 'w') as fout:
+        fout.write(c.out)
+
+      errfile = os.path.basename(f) + ".err"
+      with open(errfile, 'w') as fout:
+        fout.write(c.err)
+    os.chdir('../')
